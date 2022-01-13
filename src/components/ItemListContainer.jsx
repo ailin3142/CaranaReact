ebcfa828096d2e10e85063c65209ec2ba5a565b7
ailin3142 <ailin.ladino@gmail.com>
2022-01-13 16:30:06 -0300

    agrego carrito de compras en el navBar, agrego lista de items con saludo

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+export default function ItemListContainer({greeting}) {
+    return(
+        <>
+            <div class="greeting">
+                <h1>{greeting}</h1>
+            </div>
+        </>
+    )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
-import react from "react";
+import CartWidget from "./CartWidget"
 
 export default function NavBar() {
     const secciones = ["Inicio", "Productos", "Contacto", "Clientes", "Puntos de Entrega"]
     return(
         <>
         <div class="estatico">
-		<ul>
-            {secciones.map(seccion => <li>{seccion}</li>)}
-		</ul>
+            <ul>
+                {secciones.map(seccion => <li>{seccion}</li>)}
+                <CartWidget buys={'0'}/>
+            </ul>
 	    </div>
         </>
     )
