a93fe8796c7b8ebf9d0bbdfca0ef8c96c10a0d39
ailin3142 <ailin.ladino@gmail.com>
2022-01-20 17:43:57 -0300

    agrego las urls de ls imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,6 +3,7 @@ export default function Item({item}) {
     return(
         <>
             <div>{item.id} {item.title} {item.price}</div>
+            <img src={item.pictureUrl} alt="item" width={120} height={120} />
         </>
     )
 }
