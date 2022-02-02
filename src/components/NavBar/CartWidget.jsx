b8cbeb2e0c6cea694e464e584c776481065ad517
ailin3142 <ailin.ladino@gmail.com>
2022-02-02 13:21:16 -0300

    navbar redirige al carrito

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function CartWidget({buys}) {
     return(
         <>
-            <div class="cartWidget">
+            <div class="cartWidget" onClick= {() => window.open("/cart", "_self")}>
                 <div>
                     <img src="../carrito.png" alt="carrito" width={30} height={30} />
                 </div>
