be17787aa8ddeb6c96af761784349a805bc09067
ailin3142 <ailin.ladino@gmail.com>
2022-02-13 14:42:02 -0300

    formulario modal y manipulacion de los datos

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react"
 import { contexto } from "../cart/CartContext";
 import { Link } from "react-router-dom";
+import Modal from "../modal/Modal";
+import firebase from "firebase";
 
 export default function Cart() {
     const { removeItem, clear, carrito } = useContext(contexto);
@@ -16,31 +18,51 @@ export default function Cart() {
         height: 24
     }
 
+    let mostrarModal = true;
+
+    const actualizarMostrarModal = (mostrar) => { mostrarModal = mostrar};
+    
+    const comprar = (buyer) => {
+        const orden = {
+            buyer: buyer,
+            items: carrito,
+            total: 1500,
+            date: firebase.firestore.Timestamp.fromDate(new Date())
+        }
+        console.log(orden);
+    }
+
     return (
+        <>
+        <Modal handleClose={comprar} show={mostrarModal} >
+                    <p>Modal</p>
+                </Modal>
         <div className="greeting">
             <h1> Carrito </h1>
-            { carrito.length > 0 ?
+            {carrito.length > 0 ?
                 <>
-                <ul>
-                    {carrito.map(producto =>
-                        <li className="itemCart" key={producto.id}>
-                            <div className="carrito">
-                                
-                                <ul>
-                                    <li>Producto: {producto.descripcion}</li>
-                                    <li>Cantidad: {producto.cantidad}</li>
-                                    <li>Precio Unitario: {producto.precio}</li>
-                                </ul>
-                                <button style={estiloBoton} onClick={() => { removeItem(producto.id) }}></button>
-                            </div>
-                        </li>)}
-                </ul>
-                <h2>Precio Total: {precioTotal}</h2>
-                <button onClick={clear} className="Button"> Eliminar todo el carrito </button>
+                    <ul>
+                        {carrito.map(producto =>
+                            <li className="itemCart" key={producto.id}>
+                                <div className="carrito">
+
+                                    <ul>
+                                        <li>Producto: {producto.descripcion}</li>
+                                        <li>Cantidad: {producto.cantidad}</li>
+                                        <li>Precio Unitario: {producto.precio}</li>
+                                    </ul>
+                                    <button style={estiloBoton} onClick={() => { removeItem(producto.id) }}></button>
+                                </div>
+                            </li>)}
+                    </ul>
+                    <h2>Precio Total: {precioTotal}</h2>
+                    <button onClick={clear} className="Button"> Eliminar todo el carrito </button>
+                    <button onClick={actualizarMostrarModal(true)} className="Button"> Comprar productos </button>
                 </>
-            :
+                :
                 <Link to="/"> Ir al catalogo</Link>
             }
-        </div>
+        </div >
+        </>
     )
 }
---
+++ src/components/modal/Modal.js
@@ -0,0 +1,45 @@
+import React, { useRef } from "react"
+
+export default function Modal({ handleClose, show }) {
+    const nombreRef = useRef();
+    const telRef = useRef();
+    const emailRef = useRef();
+    const campos = [{ titulo: "Nombre y Apellido", ref: nombreRef },
+                    { titulo: "Telefono", ref: telRef },
+                    { titulo: "Email", ref: emailRef }];
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+    const inputs = campos.map(function (item) {
+        return (
+            <ul key={item.titulo}>
+                <li>
+                    <span>{item.titulo}</span>
+                    <input type="text" ref={item.ref} placeholder={item.titulo}/>
+                    <br />
+                </li>
+            </ul>
+        )
+    });
+
+    const generarComprador = () => {
+
+        if (nombreRef.current.value === "" || telRef.current.value === "" || emailRef.current.value === "") { return; }
+        const buyer = {
+            name: nombreRef.current.value,
+            phone: telRef.current.value,
+            email: emailRef.current.value
+        }
+
+        handleClose(buyer);
+    }
+    
+    return (
+        <div className={showHideClassName}>
+            <section className="modal-main">
+                <h2> Datos del cliente </h2>
+                {inputs}
+                <button type="button" onClick={generarComprador}> Realizar compra </button>
+            </section>
+        </div>
+    );
+}
